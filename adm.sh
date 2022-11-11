a45d14eab6a2550ee7f7c886633cde42b0172f0a
sayandip199309@gmail.com <sayandip199309@gmail.com>
2022-11-11 11:10:22 +0530

    feat: Switch wslg theme

--- adm.sh
+++ adm.sh
@@ -93,3 +93,7 @@ tmux source-file "${TMUX[1]}"
 
 hardlink "${NVIM[@]}"
 set_open_nvim_theme
+
+# change wslg theme
+GTK_THEME=[ "$WINTHEME" -eq 0 ] && echo "Adwaita:dark" || echo "Adwaita:light"
+export GTK_THEME
